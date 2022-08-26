50450d9ae85f138db368c48af95e875828373b20
Gabriel Madeira <gabr.madeira@gmail.com>
2022-08-26 18:48:26 -0300

    some progress in eight_queens

--- optimization/eight_queens.py
+++ optimization/eight_queens.py
@@ -1,3 +1,5 @@
+import random
+
 def evaluate(individual):
     """
     Recebe um indivíduo (lista de inteiros) e retorna o número de ataques
@@ -7,7 +9,15 @@ def evaluate(individual):
     :param individual:list
     :return:int numero de ataques entre rainhas no individuo recebido
     """
-    raise NotImplementedError  # substituir pelo seu codigo
+    attacks = 0
+    for i in range(0, len(individual)):
+        for j in range(i+1, len(individual)):
+            if ((individual[i] == individual[j]) or 
+                (individual[i]+abs(j-i) == individual[j]) or 
+                (individual[i]-abs(j-i) == individual[j])):
+                attacks += 1
+    return attacks        
+    # raise NotImplementedError  # substituir pelo seu codigo
 
 
 def tournament(participants):
@@ -17,8 +27,26 @@ def tournament(participants):
     :param participants:list - lista de individuos
     :return:list melhor individuo da lista recebida
     """
-    raise NotImplementedError  # substituir pelo seu codigo
+    melhor = participants[0]
 
+    for i in range (0, len(participants)):
+        if evaluate(participants[i]) < evaluate(melhor): ##se o atual tem menor conflitos - atualiza o melhor
+            melhor = participants[i]
+    
+    return melhor
+    #raise NotImplementedError  # substituir pelo seu codigo
+
+def elitism(pop,e):
+    """
+    Recebe uma lista com vários indivíduos e retorna os e melhores deles, com relação
+    ao numero de conflitos
+    :param participants:list - lista de individuos
+    :return:list,list melhores e individuos da lista recebida
+    """
+    pop.sort(key=lambda x: evaluate(x))
+    pop = pop[0:e]
+    
+    return pop
 
 def crossover(parent1, parent2, index):
     """
@@ -34,7 +62,15 @@ def crossover(parent1, parent2, index):
     :param index:int
     :return:list,list
     """
-    raise NotImplementedError  # substituir pelo seu codigo
+    
+    o1 = parent1[0:index] + parent2[index:8]
+    o2 = parent2[0:index] + parent1[index:8]
+
+    children = [o1,o2] 
+
+    return children
+
+    #raise NotImplementedError  # substituir pelo seu codigo
 
 
 def mutate(individual, m):
@@ -46,7 +82,14 @@ def mutate(individual, m):
     :param m:int - probabilidade de mutacao
     :return:list - individuo apos mutacao (ou intacto, caso a prob. de mutacao nao seja satisfeita)
     """
-    raise NotImplementedError  # substituir pelo seu codigo
+    
+    rand = random.uniform(0, 1)
+    if rand < m:
+        pos = random.randint(0,7)
+        num = random.randint(1,8)
+        individual[pos] = num
+    return individual
+    # raise NotImplementedError  # substituir pelo seu codigo
 
 
 def run_ga(g, n, k, m, e):
@@ -59,4 +102,40 @@ def run_ga(g, n, k, m, e):
     :param e:bool - se vai haver elitismo
     :return:list - melhor individuo encontrado
     """
-    raise NotImplementedError  # substituir pelo seu codigo
+    pop = list()
+    for i in range(0,n):
+        ind = list()
+        for j in range(0,8):
+            rand = random.randint(1,8)
+            ind.append(rand )
+        pop.append(ind)
+
+    for gen in range(0, g):
+        newPop = elitism(pop, e)
+        
+        while(len(newPop) < n):
+            participants = list()
+            for j in range(0, k):
+                participants.append(pop[random.randint(0,n-1)])
+            
+            p1 = tournament(participants)
+            participants = list()
+            for j in range(0, k):
+                participants.append(pop[random.randint(0,n-1)])
+            p2 = tournament(participants)
+            
+            index = random.randint(1,6)
+
+            cv = crossover(p1,p2,index)
+            o1 = cv[0]
+            o2 = cv[1]
+            o1 = mutate(o1,m)
+            o2 = mutate(o2,m)
+            newPop.append(o1)
+            newPop.append(o2)
+        pop = newPop
+    return tournament(pop)
+
+        
+    
+    #raise NotImplementedError  # substituir pelo seu codigo
